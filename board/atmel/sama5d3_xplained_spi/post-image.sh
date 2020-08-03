9fdfbc08574653a67ccc556d4755e2211009adc0
RickyRockRat <gpib@rickyrockrat.com>
2020-08-03 12:17:13 -0600

    Fixed post-install script args

--- board/atmel/sama5d3_xplained_spi/post-image.sh
+++ board/atmel/sama5d3_xplained_spi/post-image.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-if [ -z "$1" ]; then
+if [ -z "$2" ]; then
 	BOARD_DIR="$(dirname $0)"
 	ITS_FILE="$BOARD_DIR/uboot.its"
 else
 	ITS_FILE="$1"
 fi
+echo "$0 $@"
+echo "Running cp '$ITS_FILE' '${BINARIES_DIR}'"
+echo "PATH=$PATH"
 cp "$ITS_FILE" "${BINARIES_DIR}"
 cd "${BINARIES_DIR}"
 mkimage -f uboot.its uimage.ub

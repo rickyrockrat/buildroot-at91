16fc636761a75278128414523201dded04aa8b69
RickyRockRat <gpib@rickyrockrat.com>
2020-08-03 11:12:35 -0600

    make post image a little more robust

--- board/atmel/sama5d3_xplained_spi/post-image.sh
+++ board/atmel/sama5d3_xplained_spi/post-image.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-BOARD_DIR="$(dirname $0)"
-cp "$BOARD_DIR/uboot.its" .
+if [ -z "$1" ]; then
+	BOARD_DIR="$(dirname $0)"
+	ITS_FILE="$BOARD_DIR/uboot.its"
+else
+	ITS_FILE="$1"
+fi
+cp "$ITS_FILE" "${BINARIES_DIR}"
+cd "${BINARIES_DIR}"
 mkimage -f uboot.its uimage.ub

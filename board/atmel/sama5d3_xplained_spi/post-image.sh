39c9d9131d8bed29bf98dce5b96cd04e7e1c374d
RickyRockRat <gpib@rickyrockrat.com>
2020-08-03 12:42:18 -0600

    Initial working post-install script

--- board/atmel/sama5d3_xplained_spi/post-image.sh
+++ board/atmel/sama5d3_xplained_spi/post-image.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+TOP_DIR=$(dirname "$BR2_CONFIG")
 if [ -z "$2" ]; then
 	BOARD_DIR="$(dirname $0)"
 	ITS_FILE="$BOARD_DIR/uboot.its"
 else
-	ITS_FILE="$2"
+	ITS_FILE="$TOP_DIR/$2"
 fi
-echo "$0 $@"
-echo "Running cp '$ITS_FILE' '${BINARIES_DIR}'"
-echo "PATH=$PATH"
+#set
+#echo "$0 $@"
+#echo "Running cp '$ITS_FILE' '${BINARIES_DIR}'"
+#echo "PATH=$PATH"
 cp "$ITS_FILE" "${BINARIES_DIR}"
 cd "${BINARIES_DIR}"
 mkimage -f uboot.its uimage.ub
